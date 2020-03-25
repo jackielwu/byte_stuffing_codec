2f2d6be0f025d38265d6c8f6e7eae433a1f47cd4
jackielwu <jackielwu@gmail.com>
2020-03-24 22:04:20 -0700

    add placeholder crc

--- byte_stuffing.c
+++ byte_stuffing.c
@@ -1,5 +1,6 @@
 #include "byte_stuffing.h"
 #include <stdio.h>
+#include <stdbool.h>
 
 /**
  * Byte Stuffing Encoding routing
@@ -75,5 +76,50 @@ size_t decoding(uint8_t *inBuf, size_t len, uint8_t *outBuf)
     else
       outBuf[outBuf_i++] = inBuf[i];
   }
+  uint16_t crc = (uint16_t) outBuf[outBuf_i - 2] << 8 | outBuf[outBuf_i - 1];
+  if(crc != crc-ccitt(outBuf, outBuf_i - 2))
+    return 0;
   return outBuf_i;
+}
+
+static void init_crc_tab(void);
+static bool crc_tab_init = false;
+static uint16_t crc_tab[256];
+
+/**
+ * Generate 16-bit CRC value using crc-ccitt
+ *
+ * @param buf   pointer to input data buffer
+ * @param len   data length of input data
+ * @return 16-bit CRC value
+ **/
+uint16_t crc-ccitt(uint8_t *buf, size_t len)
+{
+  // TODO Need initial values
+  uint16_t crc = 0xFFFF;
+  if(!crc_tab_init) init_crc_tab();
+  if(buf != NULL)
+    for(size_t i = 0; i < len; i++)
+      crc = (crc << 8) ^ crc_tab[((crc >> 8) ^ (uint16_t) *buf++) & 0x00FF];
+  return crc;
+}
+
+static void init_crc_tab(void)
+{
+  uint16_t crc, c;
+  for(size_t i = 0; i < 256; i++)
+  {
+    crc = 0;
+    c = i << 8;
+    for(size_t j = 0; j < 8; j++)
+    {
+      if((crc ^ c) & 0x8000)
+        crc = (crc << 1) ^ 0x1021;
+      else
+        crc = crc << 1;
+      c = c << 1;
+    }
+    crc_tab[i] = crc;
+  }
+  crc_tab_init = true;
 }
